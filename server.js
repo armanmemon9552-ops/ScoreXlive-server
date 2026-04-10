0cdaadbb0fdd48a2cca35e9e83a7c4833ab0b818
armanmemon9552-ops <armanmemon9552@gmail.com>
2026-04-10 17:22:15 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("ScoreX Server Running 🚀");
+});
+
+app.get("/live", async (req, res) => {
+  try {
+
+    const response = await axios.get(
+      "https://cricbuzz-cricket.p.rapidapi.com/matches/v1/live",
+      {
+        headers: {
+          "x-rapidapi-host": "cricbuzz-cricket.p.rapidapi.com",
+          "x-rapidapi-key": a567d21a28mshed82c55f8ea7788p1ba38djsn193b5bbf29a9
+        }
+      }
+    );
+
+    res.json(response.data);
+
+  } catch (err) {
+    console.log("ERROR:", err.response?.data || err.message);
+
+    res.status(500).json({
+      error: err.response?.data || err.message
+    });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
